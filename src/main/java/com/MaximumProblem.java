970a12ac0b33e5bd8bb1c94297545457b78e7003
dbhide <dhanashree.bhide3@gmail.com>
2020-02-28 18:21:30 +0530

    REFACTORED: All 3 methods to one generic method

--- src/main/java/com/MaximumProblem.java
+++ src/main/java/com/MaximumProblem.java
@@ -4,33 +4,14 @@ import java.sql.SQLOutput;
 
 public class MaximumProblem {
 
-    public Integer getMaximum(Integer num1, Integer num2, Integer num3) {
-        if (num1.compareTo(num2) > 0) {
-            return num1;
-        }
-        if (num2.compareTo(num3) > 0) {
-            return num2;
-        }
-        return num3;
-    }
+    public static <E extends Comparable<E>> E getMaximum(E value1, E value2, E value3) {
 
-    public Float getMaximumFloat(Float num1, Float num2, Float num3) {
-        if (num1.compareTo(num2) > 0) {
-            return num1;
+        if (value1.compareTo(value2) > 0) {
+            return value1;
         }
-        if (num2.compareTo(num3) > 0) {
-            return num2;
+        if (value2.compareTo(value3) > 0) {
+            return value2;
         }
-        return num3;
-    }
-
-    public String getMaximumString(String a, String b, String c) {
-        if (a.compareTo(b) > 0) {
-            return a;
-        }
-        if (b.compareTo(c) > 0) {
-            return b;
-        }
-        return c;
+        return value3;
     }
 }
--- src/test/java/com/MaximumProblemTest.java
+++ src/test/java/com/MaximumProblemTest.java
@@ -30,42 +30,42 @@ public class MaximumProblemTest {
     @Test
     public void givenMaxFloatNumberAt_1stPosition_ReturnsTheNumber() {
         MaximumProblem maximumProblem = new MaximumProblem();
-        Float maximumFloat = maximumProblem.getMaximumFloat(0.5f, 0.2f, 0.1f);
+        Float maximumFloat = maximumProblem.getMaximum(0.5f, 0.2f, 0.1f);
         Assert.assertEquals(Optional.of(0.5f), Optional.of(maximumFloat));
     }
 
     @Test
     public void givenMaxFloatNumberAt_2ndPosition_ReturnsTheNumber() {
         MaximumProblem maximumProblem = new MaximumProblem();
-        Float maximumFloat = maximumProblem.getMaximumFloat(0.5f, 1.2f, 0.1f);
+        Float maximumFloat = maximumProblem.getMaximum(0.5f, 1.2f, 0.1f);
         Assert.assertEquals(Optional.of(1.2f), Optional.of(maximumFloat));
     }
 
     @Test
     public void givenMaxFloatNumberAt_3rdPosition_ReturnsTheNumber() {
         MaximumProblem maximumProblem = new MaximumProblem();
-        Float maximumFloat = maximumProblem.getMaximumFloat(0.5f, 1.2f, 2.1f);
+        Float maximumFloat = maximumProblem.getMaximum(0.5f, 1.2f, 2.1f);
         Assert.assertEquals(Optional.of(2.1f), Optional.of(maximumFloat));
     }
 
     @Test
     public void givenMaxStringAt_1stPosition_ReturnsTheString() {
         MaximumProblem maximumProblem = new MaximumProblem();
-        String maximumString = maximumProblem.getMaximumString("peach", "apple", "banana");
+        String maximumString = maximumProblem.getMaximum("peach", "apple", "banana");
         Assert.assertEquals("peach", maximumString);
     }
 
     @Test
     public void givenMaxStringAt_2ndPosition_ReturnsTheString() {
         MaximumProblem maximumProblem = new MaximumProblem();
-        String maximumString = maximumProblem.getMaximumString("apple", "peach", "banana");
+        String maximumString = maximumProblem.getMaximum("apple", "peach", "banana");
         Assert.assertEquals("peach", maximumString);
     }
 
     @Test
     public void givenMaxStringAt_3rdPosition_ReturnsTheString() {
         MaximumProblem maximumProblem = new MaximumProblem();
-        String maximumString = maximumProblem.getMaximumString("apple", "banana", "peach");
+        String maximumString = maximumProblem.getMaximum("apple", "banana", "peach");
         Assert.assertEquals("peach", maximumString);
     }
 }
